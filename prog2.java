132d8009ee714ad88decc159cadb554ee0929463
Madhunisa <nisasenthil97@gmail.com>
2017-06-06 12:26:31 +0530

    Update and rename README.md to prog2.java

---
+++ prog2.java
@@ -0,0 +1,19 @@
+# oddeven.java
+import java.io.*;
+import java.util.*;
+public class prog2	
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n%2==0)
+{
+System.out.println("the number is even");
+}
+else
+{
+System.out.println("the number is odd");
+}
+}
+}
